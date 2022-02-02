f0580d933ccff4a6bc3e8d8cfa7510de9a4f5591
Chiluka Raju <ca.raju@diacritech.local>
2022-02-02 14:39:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotesService } from './notes.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-backend-node';
+  showAddForm = false;
+  notes: any = [];
+  model:any={};
+  constructor(private notesService:NotesService) { }
+
+  ngOnInit(){
+    this.getMenusAll();
+  }
+
+
+  getMenusAll(){
+    let errorMessage = "Error Loading !!!, Please contact admin.";
+    this.notesService.getNotesAll().subscribe(
+      (response: any[]) => {
+        this.notes = response;
+    },
+    error =>{
+      this.notes=[];
+      
+    },
+    () =>{
+
+    }) ;
+  }
+
+  editNote(note){
+
+  }
+
+  deleteNote(note){
+    
+  }
+
+  addNote(){
+    this.showAddForm = true;
+  }
+
+  submitAddNote(){
+    this.showAddForm = false;
+    console.log(this.model);
+    // this.model={};
+    // this.model={"title":"3","content":"api 3"};
+    let errorMessage = "Error Loading !!!, Please contact admin.";
+    this.notesService.addNote(this.model).subscribe(
+      (response: any[]) => {
+        
+    },
+    error =>{
+      this.notes=[];
+      this.model={};
+    },
+    () =>{
+      this.model={};
+      this.getMenusAll();
+    }) ;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common'; //added this line to your module
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
